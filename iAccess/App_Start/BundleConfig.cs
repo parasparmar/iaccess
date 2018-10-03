994709662af365becf07f3b55cfa85ad1d45b3d8
Paras Parmar <paras.parmar@sitel.com>
2018-10-04 02:54:13 +0530

    Working Commit

--- iAccess/App_Start/BundleConfig.cs
+++ iAccess/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace iAccess
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bower_components/bootstrap/dist/js/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Scripts/bower_components/bootstrap/dist/css/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
