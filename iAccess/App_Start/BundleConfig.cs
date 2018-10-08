0b0f3451f4b3ee4490aac5743158f7bc8386dba8
Paras Parmar <paras.parmar@sitel.com>
2018-10-08 21:46:27 +0530

    Working Commit. Basic Layout is ok

--- iAccess/App_Start/BundleConfig.cs
+++ iAccess/App_Start/BundleConfig.cs
@@ -8,26 +8,25 @@ namespace iAccess
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
+            bundles.Add(new ScriptBundle("~/styles_bundle/").Include(
+                "~/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "~/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css",
+                "~/AdminLTE/bower_components/Ionicons/css/ionicons.min.css",
+                "~/AdminLTE/dist/css/AdminLTE.min.css",
+                "~/AdminLTE/dist/css/skins/skin-purple.min.css",
+                "~/Sitel/fonts/GoogleFontCSS.css",
+                "~/Sitel//css/iAccessStyleSheet.css"
+                ));
+
             bundles.Add(new ScriptBundle("~/scripts_bundle/").Include(
                 "~/AdminLTE/bower_components/jquery/dist/jquery.js",
                 "~/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js",
                 "~/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js",
                 "~/AdminLTE/bower_components/fastclick/lib/fastclick.js",
-                "~/AdminLTE/dist/js/adminlte.min.js",
-                "~/AdminLTE/dist/js/demo.js",
+                "~/AdminLTE/dist/js/adminlte.min.js",                
                 "~/Sitel/js/jquery.validate*"
                 ));
-
-            bundles.Add(new ScriptBundle("~/styles_bundle/").Include(
-                "~/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css",
-                "~/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css",
-                "~/AdminLTE/dist/css/AdminLTE.min.css",
-                "~/AdminLTE/bower_components/Ionicons/css/ionicons.min.css",
-                "~/AdminLTE/dist/css/skins/skin-purple.min.css",
-                "~/Sitel/js/respond.min.js",
-                "~/Sitel/css/site.css"
-                ));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/modernizr/").Include(
